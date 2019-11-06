d58005b20c90a4c710134ac19c873c5cb805a89a
nickyjjanse <i421118@fhict.nl>
2019-11-06 13:48:57 +0100

    Updated Player

--- Logic/src/main/java/com/eternitywars/Models/Player.java
+++ Logic/src/main/java/com/eternitywars/Models/Player.java
@@ -8,54 +8,18 @@ import java.util.List;
 
 public class Player extends Account
 {
-    private int hp;
-    private int blueMana;
-    private int deathEssence;
     private Deck deck;
     private List<Card> cardsInHand;
     private List<Card> cardsInDeck;
     private LobbyPlayerStatus lobbyPlayerStatus;
 
-    public Player(int id, String username, AccountStatus accountStatus, Socket socket, int hp, int blueMana, int deathEssence, Deck deck, List<Card> cardsInHand, List<Card> cardsInDeck, LobbyPlayerStatus lobbyPlayerStatus)
+    public Player(int id, String username, AccountStatus accountStatus, Socket socket, Deck deck, List<Card> cardsInHand, List<Card> cardsInDeck, LobbyPlayerStatus lobbyPlayerStatus)
     {
         super(id, username, accountStatus, socket);
-        this.hp = hp;
-        this.blueMana = blueMana;
-        this.deathEssence = deathEssence;
         this.deck = deck;
         this.cardsInHand = cardsInHand;
         this.cardsInDeck = cardsInDeck;
         this.lobbyPlayerStatus = lobbyPlayerStatus;
-    }
-
-    public int getHp()
-    {
-        return hp;
-    }
-
-    public void setHp(int hp)
-    {
-        this.hp = hp;
-    }
-
-    public int getBlueMana()
-    {
-        return blueMana;
-    }
-
-    public void setBlueMana(int blueMana)
-    {
-        this.blueMana = blueMana;
-    }
-
-    public int getDeathEssence()
-    {
-        return deathEssence;
-    }
-
-    public void setDeathEssence(int deathEssence)
-    {
-        this.deathEssence = deathEssence;
     }
 
     public Deck getDeck()
