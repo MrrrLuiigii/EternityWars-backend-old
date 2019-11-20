be8eb2e2b643f9e8c8c51d2f29218de383241eda
wiebj3 <wiebj3@gmail.com>
2019-11-20 13:19:19 +0100

    Update LobbyContainerContext.java

--- Api/src/main/java/com/eternitywars/api/DAL/Contexts/LobbyContainerContext.java
+++ Api/src/main/java/com/eternitywars/api/DAL/Contexts/LobbyContainerContext.java
@@ -1,6 +1,8 @@
 package com.eternitywars.api.DAL.Contexts;
 
+import com.eternitywars.api.Database.DatabaseConnection;
+
 public class LobbyContainerContext
 {
-
+    private DatabaseConnection dbc;
 }
