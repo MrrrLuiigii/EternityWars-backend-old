4f77b4fc5601e60b97f83ad38fbd732115f48e80
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2019-12-27 10:42:27 +0100

    deleted discovery

--- Logic/src/main/java/com/eternitywars/LogicApplication.java
+++ Logic/src/main/java/com/eternitywars/LogicApplication.java
@@ -2,17 +2,13 @@ package com.eternitywars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class LogicApplication {
-
+public class LogicApplication
+{
 	@Bean
-	@LoadBalanced
 	public RestTemplate GetRestTemplate()
 	{
 		return new RestTemplate();
@@ -21,5 +17,4 @@ public class LogicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LogicApplication.class, args);
 	}
-
 }
