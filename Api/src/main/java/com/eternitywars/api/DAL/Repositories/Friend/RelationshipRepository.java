3c2da88c9dc353ec1e902725517c8a696dec57f3
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2019-12-27 21:32:05 +0100

    Update friendstatus implemented

--- Api/src/main/java/com/eternitywars/api/DAL/Repositories/Friend/RelationshipRepository.java
+++ Api/src/main/java/com/eternitywars/api/DAL/Repositories/Friend/RelationshipRepository.java
@@ -2,16 +2,13 @@ package com.eternitywars.api.DAL.Repositories.Friend;
 
 import com.eternitywars.api.DAL.Contexts.Friend.RelationshipSqlContext;
 import com.eternitywars.api.Interfaces.Friend.IRelationshipContext;
-import com.eternitywars.api.Models.Enums.FriendStatus;
-import com.eternitywars.api.Models.Friend;
 import com.eternitywars.api.Models.Relationship;
-import com.eternitywars.api.Models.User;
 
 public class RelationshipRepository implements IRelationshipContext
 {
     private IRelationshipContext relationshipContext = new RelationshipSqlContext();
 
-    public Relationship UpdateRelationship(Relationship relationship)
+    public boolean UpdateRelationship(Relationship relationship)
     {
         return relationshipContext.UpdateRelationship(relationship);
     }
