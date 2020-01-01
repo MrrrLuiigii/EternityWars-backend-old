d6e8cf908a5c2a4cece50f7c7b632f43022a3061
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-01-01 21:40:34 +0100

    nu wel werkend?

---
+++ Api/src/main/java/com/eternitywars/api/DAL/Repositories/Lobby/LobbyRepository.java
@@ -0,0 +1,33 @@
+package com.eternitywars.api.DAL.Repositories.Lobby;
+
+import com.eternitywars.api.DAL.Contexts.Lobby.LobbySqlContext;
+import com.eternitywars.api.Interfaces.Lobby.ILobbyContext;
+import com.eternitywars.api.Models.Lobby;
+import com.eternitywars.api.Models.Player;
+
+public class LobbyRepository implements ILobbyContext
+{
+    private LobbySqlContext lobbySqlContext = new LobbySqlContext();
+
+
+
+    public boolean JoinLobby(Lobby lobby, Player player)
+    {
+        return lobbySqlContext.JoinLobby(lobby, player);
+    }
+
+    public boolean LeaveLobby(Lobby lobby, Player player)
+    {
+        return lobbySqlContext.LeaveLobby(lobby, player);
+    }
+
+    public boolean UpdatePlayerStatus(Lobby lobby, Player player)
+    {
+        return lobbySqlContext.UpdatePlayerStatus(lobby, player);
+    }
+
+    public boolean UpdatePlayerDeck(Lobby lobby, Player player)
+    {
+        return lobbySqlContext.UpdatePlayerDeck(lobby, player);
+    }
+}
